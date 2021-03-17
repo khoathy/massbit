5dc99dd62709cc4c0c5a7abc6994a2dbff414678
ThyPham <macbook@Macbooks-MBP.lan>
2021-03-17 17:33:29 +0700

    finish hero section

--- js/main.js
+++ js/main.js
@@ -24,7 +24,7 @@ $(document).ready(function(){
     
         // sticky header 
         
-        if(wScroll >= ($('.section-about').offset().top - 50)){
+        if(wScroll >= ($('.section-about').offset().top - 650)){
             $('.nav').addClass('nav__sticky');
         } else {
             $('.nav').removeClass('nav__sticky');
