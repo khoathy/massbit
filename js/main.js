258feea318414b7db4d6aeec57ec72da42487d2e
khoathy <ms.khoathy@gmail.com>
2021-04-22 17:20:20 +0700

    feat: dropdown menu for each nav item

--- js/main.js
+++ js/main.js
@@ -33,12 +33,33 @@ $(document).ready(function(){
     //menu modal
     $('.menu').click(function(){
         $('.nav__menu').toggleClass('visible');
-        console.log('visible toggle')
     });
+
     $(document).click(function(event) {
         //if click on anything except the modal itself or the "open modal" link, close the modal
-        if (!$(event.target).closest(".nav__menu,.menu").length) {
-          $("body").find(".nav__menu").removeClass("visible");
+        if (!$(event.target).closest('.nav__menu,.menu').length) {
+          $('body').find('.nav__menu').removeClass('visible');
+        //   console.log('click ouside')
+        }
+    });
+
+    //dropdown modal
+    $('.dropdown__icon, .dropdown__title').click(function(){
+        $('.dropdown__list').toggleClass('dropdown__list-visible');
+                  console.log('click ouside')
+    });
+
+    $(document).click(function(event) {
+        //if click on anything except the modal itself or the "open modal" icon, close the modal
+        if (!$(event.target).closest('.dropdown__title, .dropdown__icon').length) {
+          $('body').find('.dropdown__list').removeClass('dropdown__list-visible');
+        }
+    });
+
+    $(document).click(function(event) {
+        //if click on anything except the modal itself or the "open modal" link, close the modal
+        if (!$(event.target).closest('.nav__menu,.menu').length) {
+          $('body').find('.nav__menu').removeClass('visible');
         //   console.log('click ouside')
         }
     });
